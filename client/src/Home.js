318806167b774eb0ba1efa7bff05ca263744f5e9
Francis Li <mail@francisli.com>
2021-01-15 14:08:09 -0800

    Create a first simple AboutMe component

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,9 @@
 import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 
+import AboutMe from './AboutMe';
 import Api from './Api';
 import {useAuthContext} from './AuthContext';
-import me from './me.jpg';
 
 function Home() {
   const {user} = useAuthContext();
@@ -29,9 +29,7 @@ function Home() {
     <main className="container">
       <div className="row">
         <div className="col-md-4">
-          <img className="img-fluid mb-3" src={me} alt="My Name" />
-          <h1>My Name</h1>
-          <p>Something about me. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <AboutMe />
         </div>
         <div className="col-md-7 offset-md-1">
           {sections.map(s => {
